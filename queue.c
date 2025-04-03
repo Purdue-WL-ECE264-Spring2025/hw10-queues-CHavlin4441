a5d76bc9cc7f47d7d6284e93e1275a01cc0f41c8
Colin Havlin <havlinc@purdue.edu>
2025-04-03 18:39:49 -0400

    Update queue.c

--- queue.c
+++ queue.c
@@ -1,8 +1,23 @@
 #include "queue.h"
 #include "tile_game.h"
+#define MAX_SEEN 1000003
 
-void enqueue(struct queue *q, struct game_state state) {}
+void enqueue(struct queue *q, struct game_state state) 
+{
+  uint64_t encoded = serialize(state);
+  insert_at_tail(&q -> data, encoded);
+}
 
-struct game_state dequeue(struct queue *q) { return (struct game_state){0}; }
+struct game_state dequeue(struct queue *q) 
+{ 
+  uint64_t encoded = remove_from_head(&q -> data);
+  return(deserialize(encoded)); 
+}
 
-int number_of_moves(struct game_state start) { return 0; }
+int number_of_moves(struct game_state start) 
+{ 
+  seen = calloc(MAX_SEEN, sizeof(bool));
+  free(seen);
+  free_list(q.data);
+  return(-1); 
+}
